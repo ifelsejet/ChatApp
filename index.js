1ec211e3a399cd2b96bd3f67cdc94be3792c396f
Jatnael Montes <ifelsejet@gmail.com>
2020-04-24 22:40:08 -0700

    Installed socket.io, logs when a user is connected to the server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //Inits app to be function handler to supply to HTTP server
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 //Define route handler
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
 
 //Define port
 http.listen(3000, () => {
